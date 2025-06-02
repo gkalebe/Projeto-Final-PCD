a63da7946e61df50017475a7f65fe959a8a53251
gkalebe <gabrielsousauitec@gmail.com>
2025-06-02 08:53:34 -0300

    Testes gerais

--- concatenar.py
+++ concatenar.py
@@ -4,18 +4,23 @@ import time
 
 pasta_csv = r"C:\Programação\Python\PCD\projeto_final\Dados"
 lista_dataframe = []
-t_inicial =time.time()
+
+t_inicial = time.time()
+
 for nome_arquivo in os.listdir(pasta_csv):
     if nome_arquivo.endswith(".csv"):
         caminho = os.path.join(pasta_csv, nome_arquivo)
+        
 
-        df = pd.read_csv(caminho)
-
+        df = pd.read_csv(caminho, low_memory=False, engine='c', dtype=str)
         lista_dataframe.append(df)
 
 df_concatenando = pd.concat(lista_dataframe, ignore_index=True)
-df_concatenando.to_csv("todos_csvs.csv", index=True)
-t_final =time.time()
+
+
+df_concatenando.to_csv("todos_csvs.csv", index=False)
+
+t_final = time.time()
 
 print("Arquivos concatenados com sucesso")
-print(f"tempo total {(t_final - t_inicial)/60:.2f} minutos")
+print(f"Tempo total {(t_final - t_inicial)/60:.2f} minutos")
